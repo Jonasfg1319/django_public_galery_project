dfa271e12124d46fa8a2bed4391b037ba8ff8cea
jonasfg1319 <jonasfg1319@hotmail.com>
2021-09-06 12:58:25 -0300

    update

--- public_galery/settings.py
+++ public_galery/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-v#xwu5$ow^^wc9fnmfr-*%-#^5-4y&jt6g((=)2x+y(x+hr6b!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
